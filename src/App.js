411bcd353a6c593c6bb33303f6db8d1082b44d75
tothricsaj <tothricsaj@gmail.com>
2020-04-01 21:23:13 +0200

    stacked :poop:

--- src/App.js
+++ src/App.js
@@ -8,51 +8,83 @@ class App extends React.Component{
       numbers: [0,1,2,3,4,5,6,7,8,9],
       operators: ['+', '-', '*', '/', '='],
       operator: '',
-      leftOperandus: 0,
-      rightOperandus: 0,
-      result: 0
+      leftOperandus: null,
+      rightOperandus: null,
+      result: 0,
+      leftValue: true,
     }
 
     this.renderElements = this.renderElements.bind(this)
     this.operate = this.operate.bind(this)
+    // this.setElementsToOperate = this.setElementsToOperate.bind(this)
   }
 
   renderElements(arr=[], elemClass='defaultClass') {
+    let setElementsToOperate = () => {
+      
+      console.log('I am called')
+      console.log(this.el)
+      switch(this.elemClass) {
+        case 'calcNumber':
+          if(this.state.leftValue) {
+            this.setState({...this.state, leftOperandus: parseInt(this.el)})
+            this.setState({...this.state, leftValue: false})
+          } else {
+            this.setState({...this.state, rightOperandus: parseInt(this.el)})
+          }
+          break
+        case 'calcOperator':
+          this.setState({...this.state, operator: this.el})
+          break
+        default:
+          console.log('Fail')
+          return null
+      }
+      console.table(this.state)
+    }
+
     return arr.map((el, i) => {
+
+      this.el = el
+      this.elemClass = elemClass
+      this.i = i
+
       return (
         <li 
           key={elemClass + i} 
           className={elemClass}
-          onClick={}
+          onClick={setElementsToOperate}
         >
           {el}
         </li>
       )
     })
   }
-  
+
   operate(num1, num2, operator) {
-    let tmp;
-
-    switch(operator) {
-      case '+':
-        tmp = num1 + num2
-        break
-      case '-':
-        tmp = num1 - num2
-        break
-      case '*':
-        tmp = num1 * num2
-        break
-      case '/':
-        tmp = num1 / num2
-        break
-      default:
-        tmp = 0
+    let tmp = 0;
+    if(!!num1 && !!num2) {
+      switch(operator) {
+        case '+':
+          tmp = num1 + num2
+          break
+        case '-':
+          tmp = num1 - num2
+          break
+        case '*':
+          tmp = num1 * num2
+          break
+        case '/':
+          tmp = num1 / num2
+          break
+        default:
+          tmp = 0
+      }
     }
-
     this.setState({...this.state, result: tmp})
   }
+
+  
 
   render() {
     return (
