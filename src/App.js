ef0859df5b0cfd45a4883daee96cf71b94c4e533
tothricsaj <tothricsaj@gmail.com>
2020-04-01 20:13:58 +0200

    App is made to class

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Calculator</h2>
-      <div className="calc"></div>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Calculator</h2>
+        <div className="calc"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
