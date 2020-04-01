e6bb4487ee7a4155210ea0fa2bf7b10526e1a48f
tothricsaj <tothricsaj@gmail.com>
2020-04-01 20:25:35 +0200

    render elements function / rendered numbers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      numbers: [0,1,2,3,4,5,6,7,8,9],
+    }
+
+    this.renderElements = this.renderElements.bind(this)
+  }
+
+  renderElements(arr=[], elemClass='defaultClass') {
+    return arr.map((el, i) => {
+      return (
+        <li key={i} className={elemClass}>
+          {el}
+        </li>
+      )
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h2>Calculator</h2>
-        <div className="calc"></div>
+        <div className="calc">
+          <div className="calcDisplay"></div>
+          <ul>{this.renderElements(this.state.numbers, 'calcNumber')}</ul>
+        </div>
       </div>
     );
   }
