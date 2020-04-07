54da76ec9a5ac15dab3e4b813cd562e61c68d5be
tothricsaj <tothricsaj@gmail.com>
2020-04-07 22:25:03 +0200

    style is ready

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      numbers: [0,1,2,3,4,5,6,7,8,9],
+      numbers: [7,8,9,4,5,6,1,2,3,0],
       operators: ['+', '-', '*', '/', '='],
       operator: '',
       operandusDigits: [],
